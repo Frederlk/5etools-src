371fc961e46a3b7cb2f9fac566c0802891110b2f
Petrinich Sergey <petrinich.sergey@gmail.com>
2026-02-06 17:26:15 +0300

    added for migration files

---
+++ for-migration/index.ts
@@ -0,0 +1,11 @@
+// Portable Markdown Renderer for D&D 5e JSON entries
+// Copy this folder to another codebase - no external dependencies required
+
+export * from "./entry.js";
+export * from "./types.js";
+export * from "./tags.js";
+export * from "./table.js";
+export * from "./misc-util.js";
+export * from "./attributes.js";
+export * from "./base.js";
+export * from "./renderer.js";
